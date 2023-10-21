b164e87c299492860a5933e270b48e69c6df8042
BaigeiGu <2271007263@qq.com>
2023-10-21 16:43:22 +0800

    first commit

---
+++ vue-project/src/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+import CharacterList from './components/CharacterList.vue'
+</script>
+
+<template>
+    <CharacterList />
+</template>
+
+<style scoped></style>
---
+++ vue-project/src/components/CharacterList.vue
@@ -0,0 +1,50 @@
+<script setup>
+import { ref, onMounted } from 'vue'
+import CharacterCard from './CharacterCard.vue'
+import axios from 'axios'
+
+var character_data = {}
+var character_list = ref([])
+
+function updateData(){
+    axios.get('/characters.json').then((res) => {
+        character_data = res.data
+        character_list.value = character_data['data']
+    })
+}
+updateData()
+</script>
+
+<template>
+    <div class="characters-list">
+        <div v-for="character in character_list" :key="character.id">
+            <CharacterCard v-bind:Characterdata="character" />
+        </div>
+    </div>
+    <button @click="updateData">Refresh</button>
+</template>
+
+<style>
+.characters-list {
+    display: grid;
+    grid-template-columns: repeat(auto-fill, 20em);
+    grid-template-rows: auto;
+    justify-content: space-between;
+    justify-items: center;
+}
+
+@media screen and (max-width: 768px) {
+    .characters-list {
+        font-size: 0.8em;
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: center;
+    }
+}
+
+@media screen and (max-width: 370px) {
+    .characters-list {
+        font-size: 0.7em;
+    }
+}
+</style>
