a45101a7b34095369b2bc78cb5f58eba427124cf
白给 <2271007263A@gmail.com>
2023-10-28 21:17:08 +0800

    fix(修复无法按需导入antd的问题):

--- vite.config.js
+++ vite.config.js
@@ -3,24 +3,24 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
+import AutoImport from 'unplugin-auto-import/vite'
 import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    AutoImport({
-      resolvers: [AntDesignVueResolver()],
-    }),
-    Components({
-      resolvers: [AntDesignVueResolver({importStyle: false})],
-    }),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+    plugins: [
+        vue(),
+        AutoImport({
+            resolvers: [AntDesignVueResolver({ importStyle: false })],
+        }),
+        Components({
+            resolvers: [AntDesignVueResolver({ importStyle: false })],
+        }),
+    ],
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url))
+        }
     }
-  }
 })
