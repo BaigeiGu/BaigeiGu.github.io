0cb07573f6f43b390a8a7bf0ed63b809a09ea8b4
白给 <2271007263A@gmail.com>
2023-10-28 17:40:19 +0800

    refactor(重构页脚):

--- src/components/translate-table/Footer.vue
+++ src/components/translate-table/Footer.vue
@@ -2,10 +2,14 @@
 const props = defineProps(['la'])
 </script>
 <template>
-    <p v-if="props.la == 'en'">Data: <a href="/characters.json">characters.json</a></p>
-    <p v-if="props.la == 'zh'">数据: <a href="/characters.json">characters.json</a></p>
-    <p v-if="props.la == 'en'">Welcome to Youshouyan unofficial Discord Server! <a
-            href="https://discord.gg/XEeQCxSr">https://discord.gg/XEeQCxSr</a></p>
-    <p v-if="props.la == 'zh'">欢迎加入有兽焉非官方Discord服务器！ <a href="https://discord.gg/XEeQCxSr">https://discord.gg/XEeQCxSr</a>
-    </p>
+    <div v-if="props.la == 'en'" class="en">
+        <p>Data: <a href="/characters.json">characters.json</a></p>
+        <p>Welcome to Youshouyan unofficial Discord Server! <a
+                href="https://discord.gg/XEeQCxSr">https://discord.gg/XEeQCxSr</a></p>
+    </div>
+    <div v-if="props.la == 'zh'" class="zh">
+        <p>数据: <a href="/characters.json">characters.json</a></p>
+        <p>欢迎加入有兽焉非官方Discord服务器！ <a href="https://discord.gg/XEeQCxSr">https://discord.gg/XEeQCxSr</a>
+        </p>
+    </div>
 </template>
