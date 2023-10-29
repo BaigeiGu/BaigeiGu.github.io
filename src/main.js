448582bc8232464404f53dfe35f526ff32dec3f0
白给 <2271007263A@gmail.com>
2023-10-29 18:41:22 +0800

    fix(修复打包体积过大的问题):

--- src/main.js
+++ src/main.js
@@ -4,9 +4,7 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 
-import Antd from 'ant-design-vue';
-
 const app = createApp(App);
 
-app.use(Antd).mount('#app');
+app.mount('#app');
 
