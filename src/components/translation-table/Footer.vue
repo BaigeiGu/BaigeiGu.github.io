5e27e3bef3cb0f5cc2d67b3959944657a00fa06a
ZoruaFox <96456728+ZoruaFox@users.noreply.github.com>
2023-12-15 18:39:07 +0800

    Update Footer.vue
    fix link

--- src/components/translation-table/Footer.vue
+++ src/components/translation-table/Footer.vue
@@ -8,7 +8,7 @@ const props = defineProps(['nowlanguage'])
                 href="https://discord.gg/XEeQCxSr">https://discord.gg/XEeQCxSr</a></p>
     </div>
     <div v-if="props.nowlanguage == 'zh'" class="zh">
-        <p>数据: <a href="/characters.json">characters.json</a></p>
+        <p>数据: <a href="https://github.com/BaigeiGu/BaigeiGu.github.io/blob/main/public/translation-table/characters.json">characters.json</a></p>
         <p>欢迎加入有兽焉非官方Discord服务器！ <a href="https://discord.gg/XEeQCxSr">https://discord.gg/XEeQCxSr</a>
         </p>
     </div>
